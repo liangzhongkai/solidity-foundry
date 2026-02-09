e03720eaa93c3b7a5dc18dcf0767e748d8923b3f
lzhkai <lzhkai1111@qq.com>
2026-02-09 16:59:24 +0800

    solidity independence and nodejs independence

---
+++ src/Imports.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.20;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract MyERC20 is ERC20 {
+    constructor() ERC20("MyERC20", "MYE", 18) {
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOwnable is Ownable {
+    constructor() Ownable(msg.sender) {}
+}
