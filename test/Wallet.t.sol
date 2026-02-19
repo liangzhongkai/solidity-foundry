4b009614f99f06a9f56a8ce7f0dc1e22f96c79da
lzhkai <lzhkai1111@qq.com>
2026-02-19 20:44:51 +0800

    add send eth in wallet

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -12,7 +12,7 @@ contract WalletTest is Test {
 
     function setUp() public {
         // msg.sender = address(this)
-        wallet = new Wallet();
+        wallet = new Wallet{value: 1e18}();
     }
 
     function testSetOwner() public {
@@ -49,5 +49,41 @@ contract WalletTest is Test {
         // call made from address(this) - this will fail since msg.sender is address(1)
         vm.expectRevert("caller is not owner in setOwner");
         wallet.setOwner(address(1));
+    }
+
+    // Receive ETH from wallet
+    receive() external payable {}
+
+    // Check how much ETH available for test
+    function test_LogBalance() public view {
+        console.log("ETH balance", address(this).balance / 1e18);
+    }
+
+    function _sendETH(uint256 _amount) internal {
+        (bool success,) = payable(address(wallet)).call{value: _amount}("");
+        require(success, "send ETH failed");
+    }
+
+    function test_SendETH() public {
+        uint256 bal = address(wallet).balance;
+
+        // deal
+        deal(address(1), 100);
+        assertEq(address(1).balance, 100);
+
+        deal(address(1), 10);
+        assertEq(address(1).balance, 10);
+
+        // hoax = deal + prank
+        // deal(address, uint) - Set balance of address
+        // hoax(address, uint) - deal + prank, Sets up a prank and set balance
+        deal(address(1), 124);
+        vm.prank(address(1));
+        _sendETH(124);
+
+        hoax(address(1), 234);
+        _sendETH(234);
+
+        assertEq(address(wallet).balance, bal + 124 + 234);
     }
 }
