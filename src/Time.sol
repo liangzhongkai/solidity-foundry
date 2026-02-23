f76f95b23d2c6aae969d16b02aab7cde33a330d9
lzhkai <lzhkai1111@qq.com>
2026-02-23 11:30:28 +0800

    add test case invariant 2

--- src/Time.sol
+++ src/Time.sol
@@ -4,11 +4,11 @@ contract Auction {
     uint256 public startAt = block.timestamp + 1 days;
     uint256 public endAt = block.timestamp + 2 days;
 
-    function bid() external {
+    function bid() external view {
         require(block.timestamp >= startAt && block.timestamp < endAt, "cannot bid");
     }
 
-    function end() external {
+    function end() external view {
         require(block.timestamp >= endAt, "cannot end");
     }
 }
