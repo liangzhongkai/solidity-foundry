5c22286d02154b7db113b34c82cf1f14ed935e09
lzhkai <lzhkai1111@qq.com>
2026-03-02 12:44:30 +0800

    improve 07 with only be minted by paying particular ERC20 token

--- src/07-foundry-nft/FoundryNFT.sol
+++ src/07-foundry-nft/FoundryNFT.sol
@@ -2,50 +2,63 @@
 pragma solidity 0.8.20;
 
 import {ERC721} from "openzeppelin-contracts@5.4.0/token/ERC721/ERC721.sol";
+import {IERC20} from "openzeppelin-contracts@5.4.0/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts@5.4.0/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "openzeppelin-contracts@5.4.0/access/Ownable.sol";
 
-/// @title Foundry NFT - Mintable ERC721 with fixed price
+/// @title Foundry NFT - Mintable ERC721 with fixed price in ERC20
 /// @author Foundry
-/// @notice Users can mint NFTs by paying 0.01 ether; owner can withdraw proceeds
+/// @notice Users can mint NFTs by paying with a specific ERC20 token; owner can withdraw proceeds
 /// @dev Extends OpenZeppelin ERC721 and Ownable
 contract FoundryNFT is ERC721, Ownable {
+    using SafeERC20 for IERC20;
+
     // -------------------------------------------------------------------------
     // State variables
     // -------------------------------------------------------------------------
 
     uint256 public totalSupply;
-    uint256 public constant MINT_PRICE = 0.01 ether;
+    uint256 public constant MINT_PRICE = 10 * 10 ** 18; // 10 tokens (assuming 18 decimals)
+    IERC20 public immutable paymentToken;
 
     // -------------------------------------------------------------------------
     // Errors
     // -------------------------------------------------------------------------
 
-    error InsufficientPayment();
-    error WithdrawFailed();
+    error InsufficientAllowance();
+    error InsufficientBalance();
 
     // -------------------------------------------------------------------------
     // Constructor
     // -------------------------------------------------------------------------
 
-    constructor() ERC721("FoundryNFT", "FNFT") Ownable(msg.sender) {}
+    /// @param _paymentToken The ERC20 token address used for payment
+    constructor(address _paymentToken) ERC721("FoundryNFT", "FNFT") Ownable(msg.sender) {
+        paymentToken = IERC20(_paymentToken);
+    }
 
     // -------------------------------------------------------------------------
-    // External functions (payable first, then non-payable)
+    // External functions
     // -------------------------------------------------------------------------
 
-    /// @notice Mint a new NFT by paying MINT_PRICE
-    /// @dev Increments totalSupply and mints to msg.sender
-    function mint() external payable {
-        if (msg.value < MINT_PRICE) revert InsufficientPayment();
+    /// @notice Mint a new NFT by paying MINT_PRICE in ERC20 tokens
+    /// @dev Caller must approve this contract to spend MINT_PRICE tokens first
+    function mint() external {
+        if (paymentToken.balanceOf(msg.sender) < MINT_PRICE) revert InsufficientBalance();
+        if (paymentToken.allowance(msg.sender, address(this)) < MINT_PRICE) revert InsufficientAllowance();
+
+        paymentToken.safeTransferFrom(msg.sender, address(this), MINT_PRICE);
+
         totalSupply++;
         _mint(msg.sender, totalSupply);
     }
 
-    /// @notice Withdraw all contract balance to owner
-    /// @dev Only callable by owner; reverts if transfer fails
+    /// @notice Withdraw all payment tokens to owner
+    /// @dev Only callable by owner
     function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-        (bool ok,) = owner().call{value: balance}("");
-        if (!ok) revert WithdrawFailed();
+        uint256 balance = paymentToken.balanceOf(address(this));
+        if (balance > 0) {
+            paymentToken.safeTransfer(owner(), balance);
+        }
     }
 }
--- test/FoundryNFT.t.sol
+++ test/FoundryNFT.t.sol
@@ -2,29 +2,47 @@
 pragma solidity 0.8.20;
 
 import {Test} from "forge-std@1.14.0/Test.sol";
+import {ERC20} from "openzeppelin-contracts@5.4.0/token/ERC20/ERC20.sol";
 
 import {FoundryNFT} from "../src/07-foundry-nft/FoundryNFT.sol";
 
+/// @notice Mock ERC20 token for testing
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MTK") {
+        _mint(msg.sender, 1_000_000 * 10 ** 18);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
 contract FoundryNFTTest is Test {
     FoundryNFT private nft;
+    MockERC20 private token;
 
     address private user;
     address private owner;
 
-    uint256 constant MINT_PRICE = 0.01 ether;
+    uint256 constant MINT_PRICE = 10 * 10 ** 18; // 10 tokens
 
     receive() external payable {}
 
     function setUp() public {
         owner = address(this);
-        nft = new FoundryNFT();
+        token = new MockERC20();
+        nft = new FoundryNFT(address(token));
         user = address(0x1);
+
+        // Give user some tokens
+        token.mint(user, 1000 * 10 ** 18);
     }
 
     function test_Mint_OwnerOfAndBalanceOf() public {
-        vm.deal(user, 1 ether);
-        vm.prank(user);
-        nft.mint{value: MINT_PRICE}();
+        vm.startPrank(user);
+        token.approve(address(nft), MINT_PRICE);
+        nft.mint();
+        vm.stopPrank();
 
         // ownerOf: the address that minted owns the NFT
         assertEq(nft.ownerOf(1), user, "ownerOf should be minter");
@@ -34,42 +52,46 @@ contract FoundryNFTTest is Test {
     }
 
     function test_Mint_ContractBalanceIncreasesByPrice() public {
-        vm.deal(user, 1 ether);
-        uint256 contractBalanceBefore = address(nft).balance;
+        uint256 contractBalanceBefore = token.balanceOf(address(nft));
 
-        vm.prank(user);
-        nft.mint{value: MINT_PRICE}();
+        vm.startPrank(user);
+        token.approve(address(nft), MINT_PRICE);
+        nft.mint();
+        vm.stopPrank();
 
         assertEq(
-            address(nft).balance, contractBalanceBefore + MINT_PRICE, "contract balance should increase by mint price"
+            token.balanceOf(address(nft)),
+            contractBalanceBefore + MINT_PRICE,
+            "contract balance should increase by mint price"
         );
     }
 
     function test_Withdraw_OwnerBalanceIncreases() public {
-        vm.deal(user, 1 ether);
-        vm.prank(user);
-        nft.mint{value: MINT_PRICE}();
+        vm.startPrank(user);
+        token.approve(address(nft), MINT_PRICE);
+        nft.mint();
+        vm.stopPrank();
 
-        uint256 ownerBalanceBefore = owner.balance;
-        uint256 contractBalance = address(nft).balance;
+        uint256 ownerBalanceBefore = token.balanceOf(owner);
+        uint256 contractBalance = token.balanceOf(address(nft));
 
         nft.withdraw();
 
         assertEq(
-            owner.balance,
+            token.balanceOf(owner),
             ownerBalanceBefore + contractBalance,
-            "owner Ether balance should increase by withdrawn amount"
+            "owner token balance should increase by withdrawn amount"
         );
-        assertEq(address(nft).balance, 0, "contract balance should be 0 after withdraw");
+        assertEq(token.balanceOf(address(nft)), 0, "contract balance should be 0 after withdraw");
     }
 
     function test_MintMultiple() public {
-        vm.deal(user, 1 ether);
         vm.startPrank(user);
+        token.approve(address(nft), MINT_PRICE * 3);
 
-        nft.mint{value: MINT_PRICE}();
-        nft.mint{value: MINT_PRICE}();
-        nft.mint{value: MINT_PRICE}();
+        nft.mint();
+        nft.mint();
+        nft.mint();
 
         vm.stopPrank();
 
@@ -80,19 +102,33 @@ contract FoundryNFTTest is Test {
         assertEq(nft.balanceOf(user), 3);
     }
 
-    function test_Mint_InsufficientPaymentReverts() public {
-        vm.deal(user, 1 ether);
+    function test_Mint_InsufficientBalanceReverts() public {
+        address poorUser = address(0x2);
+        // poorUser has no tokens
+
+        vm.prank(poorUser);
+        vm.expectRevert(FoundryNFT.InsufficientBalance.selector);
+        nft.mint();
+    }
+
+    function test_Mint_InsufficientAllowanceReverts() public {
         vm.prank(user);
-        vm.expectRevert(FoundryNFT.InsufficientPayment.selector);
-        nft.mint{value: 0.001 ether}();
+        // User has tokens but didn't approve
+        vm.expectRevert(FoundryNFT.InsufficientAllowance.selector);
+        nft.mint();
     }
 
     function test_TokenMetadata() public {
-        vm.deal(user, 1 ether);
-        vm.prank(user);
-        nft.mint{value: MINT_PRICE}();
+        vm.startPrank(user);
+        token.approve(address(nft), MINT_PRICE);
+        nft.mint();
+        vm.stopPrank();
 
         assertEq(nft.name(), "FoundryNFT");
         assertEq(nft.symbol(), "FNFT");
+    }
+
+    function test_PaymentTokenIsSet() public view {
+        assertEq(address(nft.paymentToken()), address(token));
     }
 }
